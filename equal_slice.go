433b2be3d398f21c41d7fc18732e3223b3f8cc46
Matt <matthew@synesis.com.au>
2019-03-11 09:46:36 +1100

    ~ fixing copyright info

--- equal_slice.go
+++ equal_slice.go
@@ -20,7 +20,7 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the names of Matthew Wilson, Sean Kelly, Synesis Software nor
+ * - Neither the names of Matthew Wilson, Synesis Software nor
  *   the names of any contributors may be used to endorse or promote products
  *   derived from this software without specific prior written permission.
  *
