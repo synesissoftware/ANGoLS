422bbec8b0c86131030013b7bfb85561b29c4136
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-03-03 10:35:57 +1100

    **version_test.go** : ~ fixed dependencies

--- version_test.go
+++ version_test.go
@@ -1,7 +1,8 @@
-package angols
+package angols_test
 
 import (
 	"github.com/stretchr/testify/require"
+	angols "github.com/synesissoftware/ANGoLS"
 
 	"testing"
 )
@@ -14,16 +15,16 @@ const (
 )
 
 func Test_Version_Elements(t *testing.T) {
-	require.Equal(t, Expected_VersionMajor, VersionMajor)
-	require.Equal(t, Expected_VersionMinor, VersionMinor)
-	require.Equal(t, Expected_VersionPatch, VersionPatch)
-	require.Equal(t, Expected_VersionAB, VersionAB)
+	require.Equal(t, Expected_VersionMajor, angols.VersionMajor)
+	require.Equal(t, Expected_VersionMinor, angols.VersionMinor)
+	require.Equal(t, Expected_VersionPatch, angols.VersionPatch)
+	require.Equal(t, Expected_VersionAB, angols.VersionAB)
 }
 
 func Test_Version(t *testing.T) {
-	require.Equal(t, uint64(0x0000_0006_0000_4001), Version)
+	require.Equal(t, uint64(0x0000_0006_0000_4001), angols.Version)
 }
 
 func Test_Version_String(t *testing.T) {
-	require.Equal(t, "0.6.0-alpha1", VersionString())
+	require.Equal(t, "0.6.0-alpha1", angols.VersionString())
 }
